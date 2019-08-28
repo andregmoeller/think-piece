7fb2cd59a7c2e813519d5debafa8a7bbc4aa3c38
Andre Moeller <13366062+andregmoeller@users.noreply.github.com>
2019-08-28 23:21:23 +0200

    Subscribes to database updates

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -9,25 +9,25 @@ class Application extends Component {
     posts: [],
   };
 
+  unsubscribe = null;
+
   componentDidMount = async () => {
-    const snapshot = await firestore.collection('posts').get();
-    const posts = snapshot.docs.map(collectIdsAndDocs);
-    this.setState( { posts } );
+    this.unsubscribe = firestore.collection('posts').onSnapshot(snapshot => {
+      const posts = snapshot.docs.map(collectIdsAndDocs);
+      this.setState({ posts });
+    });
+  };
+
+  componentWillUnmount = () => {
+    this.unsubscribe();
   };
 
   handleCreate = async post => {
-    const { posts } = this.state;
-    const docRef = await firestore.collection('posts').add(post);
-    const doc = await docRef.get();
-    const newPost = collectIdsAndDocs(doc);
-    this.setState({ posts: [newPost, ...posts] });
+    firestore.collection('posts').add(post);
   };
 
   handleRemove = async id => {
-    const allPosts = this.state.posts;
-    await firestore.doc(`posts/${id}`).delete();
-    const posts = allPosts.filter(post => post.id !== id);
-    this.setState({ posts });
+    firestore.doc(`posts/${id}`).delete();
   };
 
   render() {
