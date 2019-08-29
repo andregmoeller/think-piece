4074f55fe6ab875bd11057ca80eb61f4a2736ec1
Andre Moeller <13366062+andregmoeller@users.noreply.github.com>
2019-08-29 17:31:29 +0200

    Makes it possible to star posts

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,6 +5,7 @@ import { firestore } from '../firebase';
 const Post = ({ id, title, content, user, createdAt, stars, comments }) => {
   const postRef = firestore.doc(`posts/${id}`);
   const remove = () => postRef.delete();
+  const star = () => postRef.update({ stars: stars + 1 })
 
   return (
     <article className="Post">
@@ -30,7 +31,7 @@ const Post = ({ id, title, content, user, createdAt, stars, comments }) => {
           <p>{moment(createdAt).calendar()}</p>
         </div>
         <div>
-          <button className="star">Star</button>
+          <button className="star" onClick={star}>Star</button>
           <button className="delete" onClick={remove}>Delete</button>
         </div>
       </div>
