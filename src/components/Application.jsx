9047db361883113efb25525d6cdaff744d9c38dd
Andre Moeller <13366062+andregmoeller@users.noreply.github.com>
2019-09-22 04:55:17 +0200

    Pulls out the user into the the React Context API.

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,33 +1,13 @@
 import React, { Component } from 'react';
-import { auth, createUserProfileDocument } from '../firebase';
 import Posts from './Posts';
 import Authentication from './Authentication';
 
 class Application extends Component {
-  state = {
-    user: null,
-  };
-
-  unssubscribeFromAuth = null;
-
-  componentDidMount = async () => {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      const user = await createUserProfileDocument(userAuth);
-      this.setState({ user });
-    });
-  };
-
-  componentWillUnmount = () => {
-    this.unssubscribeFromAuth();
-  };
-
   render() {
-    const { user } = this.state;
-
     return (
       <main className="Application">
         <h1>Think Piece</h1>
-        <Authentication user={user} />
+        <Authentication />
         <Posts />
       </main>
     );
